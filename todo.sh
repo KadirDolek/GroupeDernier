27f642227912370f232d5f22cad5e67377012962
Dilara <dilaraozturk028@gmail.com>
2025-03-07 16:26:00 +0100

    derniere modif

--- todo.sh
+++ todo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-nl "$todo.txt"
-read -p "Numéro de la tâche à supprimer : " num
-sed -i "${num}d" "$todo.txt"
-;;
-
 echo "insert text here" txt
 read txt
 echo $txt >> todo.txt
+nl todo.txt
+
+read -p "Numéro de la tâche à supprimer : " num
+sed -i "${num}d" "todo.txt"
+nl todo.txt
