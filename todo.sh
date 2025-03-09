32ef2631792982fd0cedd926fae9eefbb906a9fd
Dilara <dilaraozturk028@gmail.com>
2025-03-09 21:34:50 +0100

    fusion

--- todo.sh
+++ todo.sh
@@ -14,9 +14,23 @@ while true; do
             echo "test"
             ;;
         "2")
-            clear
-            echo "test"
-            ;;
+                nl todo.txt
+            while true; do
+                read -p "Numéro de la tâche à supprimer : " num
+            if [[ ! $num =~ ^[0-9]+$ ]]; then
+                echo "Veuillez entrer un numéro valide."
+            else
+                if [ $(wc -l < todo.txt) -lt $num ] || [ $num -le 0 ]; then
+                echo "Numéro de tâche invalide."
+            else
+                sed -i "${num}d" todo.txt
+                echo "Tâche supprimée !"
+                nl todo.txt
+            break
+                fi
+            fi
+        done
+        ;;
         "3")
             clear
             cat tasks.txt
