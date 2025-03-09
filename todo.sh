ad718ee6c50d40bdd3c4af3f819f203164547876
Dilara <dilaraozturk028@gmail.com>
2025-03-09 21:22:53 +0100

    restauration du main

--- todo.sh
+++ todo.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
+echo "Bienvenue dans la to-do liste"
+while true; do
+    echo "1.Ajouter"
+    echo "2.Supprimer"
+    echo "3.Afficher"
+    echo "4.Supprimer toute la liste"
+    echo "5.Modifier"
+    echo "6.Quitter"
+    read option
+    case $option in
+        "1")
+            clear
+            echo "test"
+            ;;
         "2")
-                nl todo.txt
-            while true; do
-                read -p "Numéro de la tâche à supprimer : " num
-            if [[ ! $num =~ ^[0-9]+$ ]]; then
-                echo "Veuillez entrer un numéro valide."
-            else
-                if [ $(wc -l < todo.txt) -lt $num ] || [ $num -le 0 ]; then
-                echo "Numéro de tâche invalide."
-            else
-                sed -i "${num}d" todo.txt
-                echo "Tâche supprimée !"
-                nl todo.txt
+            clear
+            echo "test"
+            ;;
+        "3")
+            clear
+            cat tasks.txt
+            ;;
+        "4")
+            clear
+            echo "Aurevoir"
             break
-                fi
-            fi
-        done
-        ;;
+            ;;
+        "5")
+            clear
+            echo "test"
+            ;;
+        "6")
+            clear
+            echo "test"
+            ;;
+        *)
+            clear
+            echo "cette entré n'est pas connu"
+            ;;
+    esac
+done
